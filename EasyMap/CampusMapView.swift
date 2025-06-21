5dac41d020935a4416a2f18e328c874ec39157ec
Gaetano <gaetanocarbone2004@gmail.com>
2025-06-21 13:10:31 +0200

    aggiunta mappa

---
+++ EasyMap/CampusMapView.swift
@@ -0,0 +1,18 @@
+//
+//  CampusMapView.swift
+//  EasyMap
+//
+//  Created by Studente on 21/06/25.
+//
+
+import SwiftUI
+
+struct CampusMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CampusMapView()
+}
