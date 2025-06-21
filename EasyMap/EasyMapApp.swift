d030d70995f900bd3ab8d49372cb831f5fc08b72
Gaetano <gaetanocarbone2004@gmail.com>
2025-06-21 13:06:13 +0200

    Initial Commit

---
+++ EasyMap/EasyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyMapApp.swift
+//  EasyMap
+//
+//  Created by Studente on 21/06/25.
+//
+
+import SwiftUI
+
+@main
+struct EasyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
