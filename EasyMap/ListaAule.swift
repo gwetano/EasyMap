94fe18d22b0d88b6d11c4dd47d7d3dc9e84f014f
Lorenzo <lorenzocampagna61@gmail.com>
2025-06-23 22:33:14 +0200

    listati aule

---
+++ EasyMap/ListaAule.swift
@@ -0,0 +1,18 @@
+//
+//  ListaAule.swift
+//  EasyMap
+//
+//  Created by Lorenzo Campagna on 23/06/25.
+//
+
+import SwiftUI
+
+struct ListaAule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListaAule()
+}
