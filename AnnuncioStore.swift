e84a343ec6de544cbf03cdbe283e8dbb22971f96
fra343 <francescoapicella471@gmail.com>
2025-06-24 17:14:23 +0200

    luca dello russo

---
+++ AnnuncioStore.swift
@@ -0,0 +1,18 @@
+//
+//  AnnuncioStore.swift
+//  EasyMap
+//
+//  Created by Francesco Apicella on 24/06/25.
+//
+
+import SwiftUI
+
+struct AnnuncioStore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnnuncioStore()
+}
---
+++ EasyMap/Preview Content/BachecaTikTokView.swift
@@ -0,0 +1,18 @@
+//
+//  BachecaTikTokView.swift
+//  EasyMap
+//
+//  Created by Francesco Apicella on 24/06/25.
+//
+
+import SwiftUI
+
+struct BachecaTikTokView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BachecaTikTokView()
+}
