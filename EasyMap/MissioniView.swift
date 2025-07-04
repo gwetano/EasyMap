c8c82596fd4811d46070f0d2f22479154440c0aa
fra343 <francescoapicella471@gmail.com>
2025-07-04 15:42:32 +0200

    Aggiunta sezione missioni

---
+++ EasyMap/MissioniView.swift
@@ -0,0 +1,18 @@
+//
+//  MissioniView.swift
+//  EasyMap
+//
+//  Created by Francesco Apicella on 04/07/25.
+//
+
+import SwiftUI
+
+struct MissioniView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissioniView()
+}
